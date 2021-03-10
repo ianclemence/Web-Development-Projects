6ca0aff0cd57149adfa6778b88f85fbf3aa24703
Ian Clemence <batricupid@gmail.com>
2021-03-10 18:03:33 +0300

    App Skeleton

---
+++ 6. Stopwatch App(Bootstrap & Jquery)/jquery.js
@@ -0,0 +1,36 @@
+$(function(){
+    //Variables
+        //app mode
+        //time counter
+        //lap counter
+        //variable for setInterval
+        //number of laps
+        //minutes, seconds, centiseconds for time and lap
+    
+    //On App load show start na dlap buttons
+    
+    //click on startButton
+        //mode on 
+        //show stop and lap buttons
+        //start counter
+    
+    //click on stopButton
+        //show resume and reset buttons
+        //stop counter
+    
+    //click on resumeButton
+        //show resume and reset buttons
+        //start action
+    
+    //click on resetbutton
+        //reload the page
+    
+    //click on lapButton
+        //if mode is ON
+            //stop action
+            //resetLap and print lap details
+            //start action
+    
+    //functions
+    
+})
