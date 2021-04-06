6c8eed8a6f6dc687f18550cd3ca0ca203a676a9e
Ian Clemence <batricupid@gmail.com>
2021-04-06 19:23:10 +0300

    Create File introduction.php

---
+++ 8. Google Maps APIs/1. introduction.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Introduction TO Coogle Maps Javascript API</title>
+<meta name="viewport" content="initial-scale=1, user-scaable=no">
+<meta charset="UTF-8">
+</head>
+<style>
+html, body{
+    height: 100%;
+}
+#map{
+    height:300px;
+}
+</style>
+<body>
+<div id="map"></div>
+<script src="https://maps.gogleapis.com/maps/api/js?key=AIzaSyAyCj4_2iHHOnFNhNMoSSJzNRt2mVBPBnc">
+</script>
+
+<script>
+//set map options
+var myLatLng = {lat: 51.5, lng: -0.1};
+var mapOptions = {
+    center: myLatLng,
+    zoom: 7,
+    mapTypeId: google.maps.MapTypeId.SATELLITE
+
+};
+//create map
+var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+//setting the mapTypeId upon construction
+map.setMapTypeId(google.maps.MapTypeId.ROADMAP);
+</script>
+</body>
+
+</html>
