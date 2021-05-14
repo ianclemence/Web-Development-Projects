b2a1125190f0afc053840194f2857b8d87f5b821
Ian Clemence <batricupid@gmail.com>
2021-05-14 03:33:02 +0300

    Initial Commit

--- Notes-App/mainpageloggedin.php
+++ Notes-App/mainpageloggedin.php
@@ -1,34 +1,38 @@
 <?php
 session_start();
-if(!isset($_SESSION['user_id'])){
+if (!isset($_SESSION['user_id'])) {
     header("location: index.php");
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>My Notes</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
-      <link href="styling.css" rel="stylesheet">
-      <link href='https://fonts.googleapis.com/css?family=Arvo' rel='stylesheet' type='text/css'>
-      <style>
-        #container{
-            margin-top:120px;   
+    <link href="styling.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Arvo' rel='stylesheet' type='text/css'>
+    <style>
+        #container {
+            margin-top: 120px;
         }
 
-        #notePad, #allNotes, #done, .delete{
-            display: none;   
+        #notePad,
+        #allNotes,
+        #done,
+        .delete {
+            display: none;
         }
 
-        .buttons{
-            margin-bottom: 20px;   
+        .buttons {
+            margin-bottom: 20px;
         }
 
-        textarea{
+        textarea {
             width: 100%;
             max-width: 100%;
             font-size: 16px;
@@ -38,112 +42,115 @@ if(!isset($_SESSION['user_id'])){
             color: #CA3DD9;
             background-color: #FBEFFF;
             padding: 10px;
-              
+
         }
-        
-        .noteheader{
+
+        .noteheader {
             border: 1px solid grey;
             border-radius: 10px;
             margin-bottom: 10px;
             cursor: pointer;
             padding: 0 10px;
-            background: linear-gradient(#FFFFFF,#ECEAE7);
+            background: linear-gradient(#FFFFFF, #ECEAE7);
         }
-          
-        .text{
+
+        .text {
             font-size: 20px;
             overflow: hidden;
             white-space: nowrap;
             text-overflow: ellipsis;
         }
-          
-        .timetext{
+
+        .timetext {
             overflow: hidden;
             white-space: nowrap;
             text-overflow: ellipsis;
         }
-        .notes{
+
+        .notes {
             margin-bottom: 100px;
         }
+    </style>
+</head>
 
-      </style>
-  </head>
-  <body>
-    <!--Navigation Bar-->  
-      <nav role="navigation" class="navbar navbar-custom navbar-fixed-top">
-      
-          <div class="container-fluid">
-            
-              <div class="navbar-header">
-              
-                  <a class="navbar-brand">Online Notes</a>
-                  <button type="button" class="navbar-toggle" data-target="#navbarCollapse" data-toggle="collapse">
-                      <span class="sr-only">Toggle navigation</span>
-                      <span class="icon-bar"></span>
-                      <span class="icon-bar"></span>
-                      <span class="icon-bar"></span>
-                  
-                  </button>
-              </div>
-              <div class="navbar-collapse collapse" id="navbarCollapse">
-                  <ul class="nav navbar-nav">
+<body>
+    <!--Navigation Bar-->
+    <nav role="navigation" class="navbar navbar-custom navbar-fixed-top">
+
+        <div class="container-fluid">
+
+            <div class="navbar-header">
+
+                <a class="navbar-brand">Online Notes</a>
+                <button type="button" class="navbar-toggle" data-target="#navbarCollapse" data-toggle="collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+
+                </button>
+            </div>
+            <div class="navbar-collapse collapse" id="navbarCollapse">
+                <ul class="nav navbar-nav">
                     <li><a href="profile.php">Profile</a></li>
                     <li><a href="#">Help</a></li>
                     <li><a href="#">Contact us</a></li>
-                      <li class="active"><a href="#">My Notes</a></li>
-                  </ul>
-                  <ul class="nav navbar-nav navbar-right">
-                      <li><a href="#">Logged in as <b><?php echo $_SESSION['username']?></b></a></li>
+                    <li class="active"><a href="#">My Notes</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">Logged in as <b><?php echo $_SESSION['username'] ?></b></a></li>
                     <li><a href="index.php?logout=1">Log out</a></li>
-                  </ul>
-              
-              </div>
-          </div>
-      
-      </nav>
-    
-<!--Container-->
-      <div class="container" id="container">
-          <!--Alert Message-->
-          <div id="alert" class="alert alert-danger collapse">
-              <a class="close" data-dismiss="alert">
+                </ul>
+
+            </div>
+        </div>
+
+    </nav>
+
+    <!--Container-->
+    <div class="container" id="container">
+        <!--Alert Message-->
+        <div id="alert" class="alert alert-danger collapse">
+            <a class="close" data-dismiss="alert">
                 &times;
-              </a>
-              <p id="alertContent"></p>
-          
-          </div>
-          <div class="row">
-              <div class="col-md-offset-3 col-md-6">
-                  <div class="buttons">
-                      <button id="addNote" type="button" class="btn btn-info btn-lg">Add Note</button>
-                      <button id="edit" type="button" class="btn btn-info btn-lg pull-right">Edit</button>
-                      <button id="done" type="button" class="btn green btn-lg pull-right">Done</button>
-                      <button id="allNotes" type="button" class="btn btn-info btn-lg">All Notes</button>
-                  </div>
-                  
-                  <div id="notePad">
-                      <textarea rows="10"></textarea>
-                  </div>
-                  
-                  <div id="notes" class="notes">
-<!--                  Ajax call to a php file-->
-                  </div>
-              
-              </div>
-          </div>
-      </div>
+            </a>
+            <p id="alertContent"></p>
+
+        </div>
+        <div class="row">
+            <div class="col-md-offset-3 col-md-6">
+                <div class="buttons">
+                    <button id="addNote" type="button" class="btn btn-info btn-lg">Add Note</button>
+                    <button id="edit" type="button" class="btn btn-info btn-lg pull-right">Edit</button>
+                    <button id="done" type="button" class="btn green btn-lg pull-right">Done</button>
+                    <button id="allNotes" type="button" class="btn btn-info btn-lg">All Notes</button>
+                </div>
+
+                <div id="notePad">
+                    <textarea rows="10"></textarea>
+                </div>
+
+                <div id="notes" class="notes">
+                    <!--                  Ajax call to a php file-->
+                </div>
+
+            </div>
+        </div>
+    </div>
 
     <!-- Footer-->
-      <div class="footer">
-          <div class="container">
-              <p>DevelopmentIsland.com Copyright &copy; 2015-<?php $today = date("Y"); echo $today?>.</p>
-          </div>
-      </div>
+    <div class="footer">
+        <div class="container">
+            <p>Ian Clemence Copyright &copy; 2020-<?php $today = date("Y");
+                                                    echo $today ?>.</p>
+        </div>
+    </div>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
-    <script src="mynotes.js"></script>  
-  </body>
+    <script src="mynotes.js"></script>
+</body>
+
 </html>
