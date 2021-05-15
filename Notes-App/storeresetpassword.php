61f85439fbd9ad332b0b74c517158c5684ade444
Ian Clemence <batricupid@gmail.com>
2021-05-16 00:35:34 +0300

    Initial Commit

--- Notes-App/storeresetpassword.php
+++ Notes-App/storeresetpassword.php
@@ -4,27 +4,29 @@
 session_start();
 include('connection.php');
 //If user_id or key is missing
-if(!isset($_POST['user_id']) || !isset($_POST['key'])){
-    echo '<div class="alert alert-danger">There was an error. Please click on the link you received by email.</div>'; exit;
+if (!isset($_POST['user_id']) || !isset($_POST['key'])) {
+    echo '<div class="alert alert-danger">There was an error. Please click on the link you received by email.</div>';
+    exit;
 }
 //else
-    //Store them in two variables
+//Store them in two variables
 $user_id = $_POST['user_id'];
 $key = $_POST['key'];
 $time = time() - 86400;
-    //Prepare variables for the query
+//Prepare variables for the query
 $user_id = mysqli_real_escape_string($link, $user_id);
 $key = mysqli_real_escape_string($link, $key);
-    //Run Query: Check combination of user_id & key exists and less than 24h old
+//Run Query: Check combination of user_id & key exists and less than 24h old
 $sql = "SELECT user_id FROM forgotpassword WHERE rkey='$key' AND user_id='$user_id' AND time > '$time' AND status='pending'";
 $result = mysqli_query($link, $sql);
-if(!$result){
-    echo '<div class="alert alert-danger">Error running the query!</div>'; exit;
+if (!$result) {
+    echo '<div class="alert alert-danger">Error running the query!</div>';
+    exit;
 }
 //If combination does not exist
 //show an error message
 $count = mysqli_num_rows($result);
-if($count !== 1){
+if ($count !== 1) {
     echo '<div class="alert alert-danger">Please try again.</div>';
     exit;
 }
@@ -36,29 +38,27 @@ $differentPassword = '<p><strong>Passwords don\'t match!</strong></p>';
 $missingPassword2 = '<p><strong>Please confirm your password</strong></p>';
 
 //Get passwords
-if(empty($_POST["password"])){
-    $errors .= $missingPassword; 
-}elseif(!(strlen($_POST["password"])>6
-         and preg_match('/[A-Z]/',$_POST["password"])
-         and preg_match('/[0-9]/',$_POST["password"])
-        )
-       ){
-    $errors .= $invalidPassword; 
-}else{
-    $password = filter_var($_POST["password"], FILTER_SANITIZE_STRING); 
-    if(empty($_POST["password2"])){
+if (empty($_POST["password"])) {
+    $errors .= $missingPassword;
+} elseif (!(strlen($_POST["password"]) > 6
+    and preg_match('/[A-Z]/', $_POST["password"])
+    and preg_match('/[0-9]/', $_POST["password"]))) {
+    $errors .= $invalidPassword;
+} else {
+    $password = filter_var($_POST["password"], FILTER_SANITIZE_STRING);
+    if (empty($_POST["password2"])) {
         $errors .= $missingPassword2;
-    }else{
+    } else {
         $password2 = filter_var($_POST["password2"], FILTER_SANITIZE_STRING);
-        if($password !== $password2){
+        if ($password !== $password2) {
             $errors .= $differentPassword;
         }
     }
 }
 
 //If there are any errors print error
-if($errors){
-    $resultMessage = '<div class="alert alert-danger">' . $errors .'</div>';
+if ($errors) {
+    $resultMessage = '<div class="alert alert-danger">' . $errors . '</div>';
     echo $resultMessage;
     exit;
 }
@@ -71,18 +71,18 @@ $user_id = mysqli_real_escape_string($link, $user_id);
 //Run Query: Update users password in the users table
 $sql = "UPDATE users SET password='$password' WHERE user_id='$user_id'";
 $result = mysqli_query($link, $sql);
-if(!$result){
+if (!$result) {
     echo '<div class="alert alert-danger">There was a problem storing the new password in the database!</div>';
-//    echo '<div class="alert alert-danger">' . mysqli_error($link) . '</div>';
+    //    echo '<div class="alert alert-danger">' . mysqli_error($link) . '</div>';
     exit;
 }
 
 //set the key status to "used" in the forgotpassword table to prevent the key from being used twice
 $sql = "UPDATE forgotpassword SET status='used' WHERE rkey='$key' AND user_id='$user_id'";
 $result = mysqli_query($link, $sql);
-if(!$result){
+if (!$result) {
     echo '<div class="alert alert-danger">Error running the query</div>';
-}else{
-    echo '<div class="alert alert-success">Your password has been update successfully!<a href="index.php">Login</a></div>';  
+} else {
+    echo '<div class="alert alert-success">Your password has been update successfully!<a href="index.php">Login</a></div>';
 }
 ?>
