afb2b57bed3c2da818f3db979e6e9f496d73606a
Ian Clemence <batricupid@gmail.com>
2021-05-16 01:28:34 +0300

    Initial Commit

--- Notes-App/loadnotes.php
+++ Notes-App/loadnotes.php
@@ -7,21 +7,22 @@ $user_id = $_SESSION['user_id'];
 //run a query to delete empty notes
 $sql = "DELETE FROM notes WHERE note=''";
 $result = mysqli_query($link, $sql);
-if(!$result){
-    echo '<div class="alert alert-warning">An error occured!</div>'; exit;
+if (!$result) {
+    echo '<div class="alert alert-warning">An error occured!</div>';
+    exit;
 }
 //run a query to look for notes corresponding to user_id
 $sql = "SELECT * FROM notes WHERE user_id ='$user_id' ORDER BY time DESC";
 
 //shows notes or alert message
-if($result = mysqli_query($link, $sql)){
-    if(mysqli_num_rows($result)>0){
-        while($row = mysqli_fetch_array($result, MYSQL_ASSOC)){
+if ($result = mysqli_query($link, $sql)) {
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_array($result, MYSQL_ASSOC)) {
             $note_id = $row['id'];
             $note = $row['note'];
             $time = $row['time'];
             $time = date("F d, Y h:i:s A", $time);
-            
+
             echo "
         <div class='note'>
             <div class='col-xs-5 col-sm-3 delete'>
@@ -34,13 +35,13 @@ if($result = mysqli_query($link, $sql)){
             </div>
         </div>";
         }
-    }else{
-        echo '<div class="alert alert-warning">You have not created any notes yet!</div>'; exit;
+    } else {
+        echo '<div class="alert alert-warning">You have not created any notes yet!</div>';
+        exit;
     }
-    
-}else{
-    echo '<div class="alert alert-warning">An error occured!</div>'; exit;
-//    echo "ERROR: Unable to excecute: $sql." . mysqli_error($link);
+} else {
+    echo '<div class="alert alert-warning">An error occured!</div>';
+    exit;
+    //    echo "ERROR: Unable to excecute: $sql." . mysqli_error($link);
 }
-
 ?>
