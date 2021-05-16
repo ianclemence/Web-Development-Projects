14601ea912f56e64424bc6134739847fea17400a
Ian Clemence <batricupid@gmail.com>
2021-05-16 04:36:44 +0300

    Initial Commit

--- Notes-App/updateemail.php
+++ Notes-App/updateemail.php
@@ -1,7 +1,7 @@
 <?php
 //start session and connect
 session_start();
-include ('connection.php');
+include('connection.php');
 
 //get user_id and new email sent through Ajax
 $user_id = $_SESSION['user_id'];
@@ -11,8 +11,9 @@ $newemail = $_POST['email'];
 $sql = "SELECT * FROM users WHERE email='$newemail'";
 $result = mysqli_query($link, $sql);
 $count = $count = mysqli_num_rows($result);
-if($count>0){
-    echo "<div class='alert alert-danger'>There is already as user registered with that email! Please choose another one!</div>"; exit;
+if ($count > 0) {
+    echo "<div class='alert alert-danger'>There is already as user registered with that email! Please choose another one!</div>";
+    exit;
 }
 
 
@@ -22,11 +23,12 @@ $result = mysqli_query($link, $sql);
 
 $count = mysqli_num_rows($result);
 
-if($count == 1){
-    $row = mysqli_fetch_array($result, MYSQL_ASSOC); 
-    $email = $row['email']; 
-}else{
-    echo "<div class='alert alert-danger'>There was an error retrieving the email from the database</div>";exit;   
+if ($count == 1) {
+    $row = mysqli_fetch_array($result, MYSQL_ASSOC);
+    $email = $row['email'];
+} else {
+    echo "<div class='alert alert-danger'>There was an error retrieving the email from the database</div>";
+    exit;
 }
 
 //create a unique activation code
@@ -35,17 +37,15 @@ $activationKey = bin2hex(openssl_random_pseudo_bytes(16));
 //insert new activation code in the users table
 $sql = "UPDATE users SET activation2='$activationKey' WHERE user_id = '$user_id'";
 $result = mysqli_query($link, $sql);
-if(!$result){
-    echo "<div class='alert alert-danger'>There was an error inserting the user details in the database.</div>";exit;
-}else{
+if (!$result) {
+    echo "<div class='alert alert-danger'>There was an error inserting the user details in the database.</div>";
+    exit;
+} else {
     //send email with link to activatenewemail.php with current email, new email and activation code
     $message = "Please click on this link prove that you own this email:\n\n";
-$message .= "http://mynotes.thecompletewebhosting.com/activatenewemail.php?email=" . urlencode($email) . "&newemail=" . urlencode($newemail) . "&key=$activationKey";
-if(mail($newemail, 'Email Update for you Online Notes App', $message, 'From:'.'developmentisland@gmail.com')){
-       echo "<div class='alert alert-success'>An email has been sent to $newemail. Please click on the link to prove you own that email address.</div>";
+    $message .= "http://mynotes.thecompletewebhosting.com/activatenewemail.php?email=" . urlencode($email) . "&newemail=" . urlencode($newemail) . "&key=$activationKey";
+    if (mail($newemail, 'Email Update for you Online Notes App', $message, 'From:' . 'developmentisland@gmail.com')) {
+        echo "<div class='alert alert-success'>An email has been sent to $newemail. Please click on the link to prove you own that email address.</div>";
+    }
 }
-    
-}
-
-
 ?>
