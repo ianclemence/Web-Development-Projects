8f89ec48ae0a18cec46f87c65a288f8d0560e3cc
Ian Clemence <batricupid@gmail.com>
2021-05-16 04:36:54 +0300

    Initial Commit

--- Notes-App/updatepassword.php
+++ Notes-App/updatepassword.php
@@ -1,7 +1,7 @@
 <?php
 //start session and connect
 session_start();
-include ('connection.php');
+include('connection.php');
 
 //define error messages
 $missingCurrentPassword = '<p><strong>Please enter your Current Password!</strong></p>';
@@ -12,66 +12,60 @@ $differentPassword = '<p><strong>Passwords don\'t match!</strong></p>';
 $missingPassword2 = '<p><strong>Please confirm your password</strong></p>';
 
 //check for errors
-if(empty($_POST["currentpassword"])){
+if (empty($_POST["currentpassword"])) {
     $errors .= $missingCurrentPassword;
-}else{
+} else {
     $currentPassword = $_POST["currentpassword"];
     $currentPassword = filter_var($currentPassword, FILTER_SANITIZE_STRING);
-    $currentPassword = mysqli_real_escape_string ($link, $currentPassword);
+    $currentPassword = mysqli_real_escape_string($link, $currentPassword);
     $currentPassword = hash('sha256', $currentPassword);
     //check if given password is correct
     $user_id = $_SESSION["user_id"];
     $sql = "SELECT password FROM users WHERE user_id='$user_id'";
     $result = mysqli_query($link, $sql);
     $count = mysqli_num_rows($result);
-    if($count !== 1){
+    if ($count !== 1) {
         echo '<div class="alert alert-danger">There was a problem running the query</div>';
-    }else{
+    } else {
         $row = mysqli_fetch_array($result, MYSQL_ASSOC);
-        if($currentPassword != $row['password']){
+        if ($currentPassword != $row['password']) {
             $errors .= $incorrectCurrentPassword;
         }
     }
-    
 }
 
-if(empty($_POST["password"])){
-    $errors .= $missingPassword; 
-}elseif(!(strlen($_POST["password"])>6
-         and preg_match('/[A-Z]/',$_POST["password"])
-         and preg_match('/[0-9]/',$_POST["password"])
-        )
-       ){
-    $errors .= $invalidPassword; 
-}else{
-    $password = filter_var($_POST["password"], FILTER_SANITIZE_STRING); 
-    if(empty($_POST["password2"])){
+if (empty($_POST["password"])) {
+    $errors .= $missingPassword;
+} elseif (!(strlen($_POST["password"]) > 6
+    and preg_match('/[A-Z]/', $_POST["password"])
+    and preg_match('/[0-9]/', $_POST["password"]))) {
+    $errors .= $invalidPassword;
+} else {
+    $password = filter_var($_POST["password"], FILTER_SANITIZE_STRING);
+    if (empty($_POST["password2"])) {
         $errors .= $missingPassword2;
-    }else{
+    } else {
         $password2 = filter_var($_POST["password2"], FILTER_SANITIZE_STRING);
-        if($password !== $password2){
+        if ($password !== $password2) {
             $errors .= $differentPassword;
         }
     }
 }
 
 //if there is an error print error message
-if($errors){
+if ($errors) {
     $resultMessage = "<div class='alert alert-danger'>$errors</div>";
-    echo $resultMessage;   
-}else{
+    echo $resultMessage;
+} else {
     $password = mysqli_real_escape_string($link, $password);
     $password = hash('sha256', $password);
     //else run query and update password
     $sql = "UPDATE users SET password='$password' WHERE user_id='$user_id'";
     $result = mysqli_query($link, $sql);
-    if(!$result){
+    if (!$result) {
         echo "<div class='alert alert-danger'>The password could not be reset. Please try again later.</div>";
-    }else{
+    } else {
         echo "<div class='alert alert-success'>Your password has been updated successfully.</div>";
     }
-    
 }
-
-
 ?>
